f35418a5d0d699ed174be07732122e7eac65864b
Đỗ Quang Đức <ducgets@gmail.com>
2020-04-05 19:58:08 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -12,24 +12,17 @@ use Illuminate\Support\Facades\Auth;
 | contains the "web" middleware group. Now create something great!
 |
 */
-    Route::post('testEmail', 'Controller@testEmail');
-    Route::get('testEmail', 'Controller@testEmail');
-Route::get('/', function () {
-    return view('welcome');
-});
-Auth::routes(['verify' => true]);
-Route::get('logout',[ 'as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
-// Auth::routes();
-Route::get('login', ['as' => 'login', 'uses' => 'Auth\LoginController@getlogin']);
-Route::post('login', ['as' => 'login', 'uses' => 'Auth\LoginController@postlogin']);
-Route::post('store',[ 'as' => 'store', 'uses' =>  'Auth\RegisterController@store']);
-Route::get('register', ['as' => 'register', 'uses' => 'Auth\RegisterController@getRegister']);
-Route::group(['prefix' => 'admin'], function () {
-    Route::resource('blog', 'admin\BlogController');
-    Route::resource('user', 'admin\UserController');
-    Route::resource('products', 'admin\ProductController');
-    Route::resource('giaovien', 'admin\TeacherController');
-
+Route::group(['prefix' => '/'], function () {
+    // Route::resource('raovat', 'index\RaovatController');
+    Route::resource('index', 'index\IndexController');
+    Route::resource('admin', 'index\UserController');
+    Route::get('admin.reset', ['as' => 'admin.reset', 'uses' => 'index\UserController@Reset']);
+    Route::get('admin.register', ['as' => 'admin.register', 'uses' => 'index\UserController@Register']);
+    Route::resource('review', 'index\ReviewController');
+    Route::resource('ad', 'index\AdController');
+    Route::resource('document', 'index\DocumentController');
+    Route::resource('teacher', 'index\TeacherController');
+    //Route::resource('review.edit', ['as' => 'review.edit', 'uses' => 'index\ReviewController@edit']);
 });
 Route::get('user/activation/{token}', 'Auth\RegisterController@activateUser')->name('user.activate');
 Route::group(['prefix' => 'index'], function () {
