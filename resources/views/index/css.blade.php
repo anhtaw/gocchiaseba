a1e8be5596bfa91a677bf06a71c68a7914bd8fe1
Đỗ Quang Đức <ducgets@gmail.com>
2020-04-15 08:11:45 +0700

    a

--- resources/views/index/css.blade.php
+++ resources/views/index/css.blade.php
@@ -309,7 +309,7 @@
     <link property="stylesheet" rel='stylesheet' id='essential-grid-plugin-settings-css' href="<?php echo asset('index/settings.css')?>" type='text/css' media='all'/>
     <link property="stylesheet" rel='stylesheet' id='tp-fontello-css' href="<?php echo asset('index/fontello.css')?>" type='text/css' media='all'/>
     <link property="stylesheet" rel='stylesheet' id='rs-plugin-settings-css' href="<?php echo asset('index/rs6.css')?>"  type='text/css' media='all'/>
-    <style id='rs-plugin-settings-inline-css' type='text/css'>
+    <style id='rs-plugin-settings-inline-css'>
         #rs-demo-id {
         }
     </style>
@@ -324,7 +324,7 @@
     <link property="stylesheet" rel='stylesheet' id='woocommerce-layout-css' href="<?php echo asset('index/woocommerce-layout.css')?>" type='text/css' media='all'/>
     <link property="stylesheet" rel='stylesheet' id='woocommerce-smallscreen-css' href="<?php echo asset('index/woocommerce-smallscreen.css')?>" type='text/css' media='only screen and (max-width: 768px)'/>
     <link property="stylesheet" rel='stylesheet' id='woocommerce-general-css' href="<?php echo asset('index/woocommerce.css')?>" type='text/css' media='all'/>
-    <style id='woocommerce-inline-inline-css' type='text/css'>
+    <style id='woocommerce-inline-inline-css' >
         .woocommerce form .form-row .required {
             visibility: visible;
         }
@@ -341,19 +341,19 @@
     <link property="stylesheet" rel='stylesheet' id='mediaelement-css' href="<?php echo asset('index/mediaelementplayer-legacy.min.css')?>" type='text/css' media='all'/>
     <link property="stylesheet" rel='stylesheet' id='wp-mediaelement-css' href="<?php echo asset('index/wp-mediaelement.min.css')?>" type='text/css' media='all'/>
     <link property="stylesheet" rel='stylesheet' id='mendel-responsive-css' href="<?php echo asset('index/responsive.css')?>" type='text/css' media='all'/>
-    <script type='text/javascript' src='{{asset('css/jquery/jqueryver1.12.4.js')}}'></script>
-    <script type='text/javascript' src='{{asset('css/jquery/jquery-migrate.minver1.4.1.js')}}'></script>
-    <script type='text/javascript' src='{{asset('css/jquery/jquery.easing.1.3ver5.3.2.js')}}'></script>
-    <script type='text/javascript' src='{{asset('css/jquery/query.timelinever=5.3.2.js')}}'></script>
-    <script type='text/javascript' src='{{asset('css/jquery/jquery.mousewheel.minver=5.3.2.js')}}'></script>
-    <script type='text/javascript' src='{{asset('css/jquery/jquery.mCustomScrollbar.minver=5.3.2.js')}}'></script>
-    <script type='text/javascript' src='{{asset('css/jquery/rolloverver=5.3.2.js')}}'></script>
-    <script type='text/javascript' src='{{asset('css/jquery/jquery.prettyPhotover=5.3.2.js')}}'></script>
-    <script type='text/javascript' src='{{asset('css/jquery/jquery.my_add_functionver=5.3.2.js')}}'></script>
-    <script type='text/javascript' src='{{asset('css/jquery/revolution.tools.minver=6.0.js')}}'></script>
-    <script type='text/javascript' src='{{asset('css/jquery/rs6.minver=6.1.3.js')}}'></script>
-    <script type='text/javascript' src='{{asset('css/jquery/jquery.blockUI.minver=2.70.js')}}'></script>
-    <script type='text/javascript'>
+    <script  src='{{asset('css/jquery/jqueryver1.12.4.js')}}'></script>
+    <script  src='{{asset('css/jquery/jquery-migrate.minver1.4.1.js')}}'></script>
+    <script  src='{{asset('css/jquery/jquery.easing.1.3ver5.3.2.js')}}'></script>
+    <script  src='{{asset('css/jquery/query.timelinever=5.3.2.js')}}'></script>
+    <script  src='{{asset('css/jquery/jquery.mousewheel.minver=5.3.2.js')}}'></script>
+    <script  src='{{asset('css/jquery/jquery.mCustomScrollbar.minver=5.3.2.js')}}'></script>
+    <script  src='{{asset('css/jquery/rolloverver=5.3.2.js')}}'></script>
+    <script  src='{{asset('css/jquery/jquery.prettyPhotover=5.3.2.js')}}'></script>
+    <script  src='{{asset('css/jquery/jquery.my_add_functionver=5.3.2.js')}}'></script>
+    <script  src='{{asset('css/jquery/revolution.tools.minver=6.0.js')}}'></script>
+    <script  src='{{asset('css/jquery/rs6.minver=6.1.3.js')}}'></script>
+    <script  src='{{asset('css/jquery/jquery.blockUI.minver=2.70.js')}}'></script>
+    <script >
         /* <![CDATA[ */
         var wc_add_to_cart_params = {
             "ajax_url": "\/wp-admin\/admin-ajax.php",
@@ -365,8 +365,8 @@
         };
         /* ]]> */
     </script>
-    <script type='text/javascript' src='{{asset('css/jquery/add-to-cart.minver=4.0.1.js')}}'></script>
-    <script type='text/javascript' src='{{asset('css/jquery/woocommerce-add-to-cartver=6.0.5.js')}}'></script>
+    <script  src='{{asset('css/jquery/add-to-cart.minver=4.0.1.js')}}'></script>
+    <script  src='{{asset('css/jquery/woocommerce-add-to-cartver=6.0.5.js')}}'></script>
     {{-- <link rel='https://api.w.org/' href='http://mendel-antiques.ancorathemes.com/wp-json/'/> --}}
     {{-- <link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://mendel-antiques.ancorathemes.com/xmlrpc.php?rsd"/> --}}
     <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="<?php echo asset('css/wlwmanifest.xml')?>"/>
